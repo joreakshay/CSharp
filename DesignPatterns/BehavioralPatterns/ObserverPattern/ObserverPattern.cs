13d01af581e956d6867d3dcfa4c97d0d80a91d51
Akshay Jore <joreakshay@gamil.com>
2026-07-22 19:18:33 +0530

    Added Observer Pattern

---
+++ DesignPatterns/BehavioralPatterns/ObserverPattern/ObserverPattern.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns.ObserverPattern
+{
+    //Subject class Chanel
+    public class Chanel
+    {
+        private List<ISubscriber> subscribers=new List<ISubscriber>();
+        private string currentVideoName;
+        public void Subscribe(ISubscriber subscriber)
+        {
+            if(subscribers.Contains(subscriber)) return;
+            subscribers.Add(subscriber);
+        }
+        public void UnSubscribe(ISubscriber subscriber)
+        {
+            if (!subscribers.Contains(subscriber)) return;
+            subscribers.Remove(subscriber);
+        }
+        public void AddNewVideo(string videoName)
+        {
+            currentVideoName = videoName;
+            UpdateAll();
+        }
+
+        private void UpdateAll()
+        {
+            foreach(ISubscriber subscriber in subscribers)
+            {
+                subscriber.SendNotification(currentVideoName);
+            }
+        }
+    }
+
+    public interface ISubscriber
+    {
+        void SendNotification(string message);
+    }
+
+    public class Subscriber : ISubscriber
+    {
+        private string userName;
+        public Subscriber(string nmae)
+        {
+            userName = nmae;
+        }
+        public void SendNotification(string message)
+        {
+            Console.WriteLine("Recived notification for {0} Added video {1}",userName, message);
+        }
+    }
+
+    public class ObserverPattern
+    {
+        public static void Main()
+        {
+            Chanel abcVideo = new Chanel();
+            ISubscriber s1=new Subscriber("Akshay");
+            ISubscriber s2 = new Subscriber("Aarti");
+            ISubscriber s3 = new Subscriber("Siya");
+
+            abcVideo.Subscribe(s1);
+            abcVideo.Subscribe(s2);
+            abcVideo.Subscribe(s3);
+
+            abcVideo.AddNewVideo("Tom and Jerry");
+
+            abcVideo.UnSubscribe(s3);
+
+            abcVideo.AddNewVideo("Pune news");
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.BehavioralPatterns.ObserverPattern;
 using DesignPatterns.CreationalPatterns.FactoryPatterns;
+using DesignPatterns.StructuralPatterns.AdapterPattern;
 
 Console.WriteLine("Hello, World!");
-FactoryPattern.Main();
+//FactoryPattern.Main();
+//AdapterPattern.Main();
+ObserverPattern.Main();
